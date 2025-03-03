a82aa84f9ade854124a0d0965ff139f9294f086b
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-03 12:47:49 -0500

    Target panels on button click and update hidden value

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ const panelButtons = document.querySelectorAll('[class*=panel-control-')
 
 panelButtons.forEach((button, i) => {
     button.addEventListener('click', function (e) {
+        // find the button parent element
+        const panelHeading = button.parentElement
+        // find the expanded state of the current button
         const expanded = button.getAttribute('aria-expanded') === 'true'
+        // set the aria-expanded attribute to the opposite of the currently expanded state
         button.setAttribute('aria-expanded', !expanded)
+        // target the panel that the button controls and add the currently expanded state to toggle hiding it
+        panelHeading.nextElementSibling.setAttribute('aria-hidden', expanded)
     })
 
     button.addEventListener('keydown', function(e) {
         if(e.key === 'ArrowUp' && (i - 1) >= 0) {
-            console.log(panelButtons[i -1])
             panelButtons[i - 1].focus()
         }
 
